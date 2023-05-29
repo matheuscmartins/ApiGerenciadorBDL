a5447c08bab0788fec539160ccb94daba2277d76
matheuscmartins <matheus87martins@outlook.com>
2023-05-29 11:07:40 -0300

    "MemberPatch Class created"

---
+++ src/main/java/br/com/brigadadoslobos/gerenciador/domains/MemberPatch.java
@@ -0,0 +1,16 @@
+package br.com.brigadadoslobos.gerenciador.domains;
+
+import java.time.LocalDate;
+
+public class MemberPatch {
+
+    private Integer id;
+    public String description;
+    private LocalDate admissionDate;
+    private Member Member;
+    private Patch patch;
+    public MemberPatch(){
+
+    }
+
+}
