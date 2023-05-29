b487c32f0ab0b1a531630f7ed0ee31c7b972da68
matheuscmartins <matheus87martins@outlook.com>
2023-05-29 13:12:11 -0300

    "BloodType Constructor, getters and setters created"

--- src/main/java/br/com/brigadadoslobos/gerenciador/domains/BloodType.java
+++ src/main/java/br/com/brigadadoslobos/gerenciador/domains/BloodType.java
@@ -1,6 +1,16 @@
 package br.com.brigadadoslobos.gerenciador.domains;
 
-public class BloodType {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class BloodType implements Serializable {
+    private  static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String description;
 
