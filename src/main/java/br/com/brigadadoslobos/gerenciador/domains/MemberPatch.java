481f19a13e7bca9adccdb78413e3a27b4a7090b2
matheuscmartins <matheus87martins@outlook.com>
2023-05-29 13:13:30 -0300

    "MemberPatch Constructor, getters and setters, created"

--- src/main/java/br/com/brigadadoslobos/gerenciador/domains/MemberPatch.java
+++ src/main/java/br/com/brigadadoslobos/gerenciador/domains/MemberPatch.java
@@ -1,16 +1,74 @@
 package br.com.brigadadoslobos.gerenciador.domains;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
+@Entity
+public class MemberPatch  implements Serializable {
+    private  static final long serialVersionUID = 1L;
 
-public class MemberPatch {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     public String description;
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate admissionDate;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
     private Member Member;
+    @ManyToOne
+    @JoinColumn(name = "patch_id")
     private Patch patch;
     public MemberPatch(){
 
     }
 
+    public MemberPatch(Integer id, String description, LocalDate admissionDate) {
+        this.id = id;
+        this.description = description;
+        this.admissionDate = admissionDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public br.com.brigadadoslobos.gerenciador.domains.Member getMember() {
+        return Member;
+    }
+
+    public void setMember(br.com.brigadadoslobos.gerenciador.domains.Member member) {
+        Member = member;
+    }
+
+    public Patch getPatch() {
+        return patch;
+    }
+
+    public void setPatch(Patch patch) {
+        this.patch = patch;
+    }
 }
