96138be0de9b1b990e0759d706ce3c45924afaca
matheuscmartins <matheus87martins@outlook.com>
2023-05-29 13:12:34 -0300

    "City Constructor, getters and setters created"

--- src/main/java/br/com/brigadadoslobos/gerenciador/domains/City.java
+++ src/main/java/br/com/brigadadoslobos/gerenciador/domains/City.java
@@ -1,7 +1,49 @@
 package br.com.brigadadoslobos.gerenciador.domains;
 
-public class City {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class City implements Serializable {
+    private  static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private String uf;
+    @ManyToOne
+    @JoinColumn(name = "uf_id")
+    private Uf uf;
+
+    public City(){
+
+    }
+
+    public City(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Uf getUf() {
+        return uf;
+    }
+
+    public void setUf(Uf uf) {
+        this.uf = uf;
+    }
 }
