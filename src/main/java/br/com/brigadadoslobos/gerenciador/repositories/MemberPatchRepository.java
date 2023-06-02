3dd6424a620366a70ddb5e5db693dc758cfbf298
matheuscmartins <matheus87martins@outlook.com>
2023-06-02 17:36:31 -0300

    "MemberPatch Repository added"

---
+++ src/main/java/br/com/brigadadoslobos/gerenciador/repositories/MemberPatchRepository.java
@@ -0,0 +1,7 @@
+package br.com.brigadadoslobos.gerenciador.repositories;
+
+import br.com.brigadadoslobos.gerenciador.domains.MemberPatch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPatchRepository extends JpaRepository<MemberPatch, Integer> {
+}
