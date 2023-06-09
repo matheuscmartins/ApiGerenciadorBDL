6c4047bde5477e90704079993306bafa70083d83
matheuscmartins <matheus87martins@outlook.com>
2023-06-09 17:29:30 -0300

    "MemberResource Created"

---
+++ src/main/java/br/com/brigadadoslobos/gerenciador/resources/MemberResource.java
@@ -0,0 +1,23 @@
+package br.com.brigadadoslobos.gerenciador.resources;
+
+import br.com.brigadadoslobos.gerenciador.domains.Member;
+import br.com.brigadadoslobos.gerenciador.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/membros")
+public class MemberResource {
+    @Autowired
+    private MemberService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Member> findById(@PathVariable Integer id) {
+        Member obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
